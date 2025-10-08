3bb772aeb0eff5dc47c01af9e9fef9d4bf9ecb45
Nikita <nikitatsarenko7@gmail.com>
2025-10-08 21:30:15 +0300

    feat(cors): add cors in main.ts

--- src/main.ts
+++ src/main.ts
@@ -5,23 +5,28 @@ import { ConfigService } from '@nestjs/config';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+    const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
-  const configService = app.get(ConfigService);
-  const port = configService.getOrThrow<number>('API_PORT');
+    const configService = app.get(ConfigService);
+    const port = configService.getOrThrow<number>('API_PORT');
 
-  app.setGlobalPrefix('v1');
-  app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: true,
-      transform: true,
-      transformOptions: {
-        enableImplicitConversion: true,
-      },
-    }),
-  );
+    app.setGlobalPrefix('v1');
+    app.useGlobalPipes(
+        new ValidationPipe({
+            whitelist: true,
+            transform: true,
+            transformOptions: {
+                enableImplicitConversion: true,
+            },
+        }),
+    );
 
-  await app.listen(port);
-  console.log(`App is running on port ${port}`);
+    app.enableCors({
+        origin: true,
+        credentials: true,
+    });
+
+    await app.listen(port);
+    console.log(`App is running on port ${port}`);
 }
 bootstrap();
